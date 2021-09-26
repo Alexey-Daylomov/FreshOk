37bb362349ea7bba3aceb963f762758dadf3108e
Alexey Daylomov <a.a.daylomov@mail.ru>
2021-09-26 13:43:02 +0700

    add slider

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+  $('.hero__slider').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+  });
+
+});
