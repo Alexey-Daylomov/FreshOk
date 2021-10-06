b68bbb04562262749a49e7d78e7244734b2d0157
Alexey Daylomov <a.a.daylomov@mail.ru>
2021-10-06 22:50:46 +0700

    responsive ready except for the header

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,14 @@ $(function () {
   $('.discounts-slider__inner').slick({
     prevArrow: '<button type="button" class="slick-btn slick-prev"><svg><use xlink:href="images/sprite.svg#arrow-gray-l"></use></svg></button>',
     nextArrow: '<button type="button" class="slick-btn slick-next"><svg><use xlink:href="images/sprite.svg#arrow-gray-r"></use></svg></button>',
+    responsive: [{
+      breakpoint: 1550,
+      settings: {
+        arrows: false,
+        dots: true,
+        autoplay: true,
+      }
+    }]
   });
 
   $('.partners__list').slick({
@@ -9,6 +17,21 @@ $(function () {
     slidesToScroll: 6,
     arrows: false,
     autoplay: true,
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      }
+    ]
   });
 
   $('.product-top__star').rateYo({
@@ -19,9 +42,20 @@ $(function () {
     readOnly: true,
   });
 
+  const button = document.querySelector('.control__network--search');
+
+  button.addEventListener('click', () => {
+    $('.form').toggleClass('form-open');
+  });
+
+  $('.menu__btn, .menu__mobile').on('click', function () {
+    $('.menu__mobile').toggleClass('menu__mobile--active');
+    $('.menu__btn').toggleClass('menu__btn--active');
+  });
+
   var containerEl1 = document.querySelector('[data-ref="product-top"]');
   var containerEl2 = document.querySelector('[data-ref="stock"]');
- 
+
   var config = {
     controls: {
       scope: 'local'
