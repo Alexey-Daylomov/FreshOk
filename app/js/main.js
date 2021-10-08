30c6f96cf61f22a35ee38943b0e5df69656e0306
Alexey Daylomov <a.a.daylomov@mail.ru>
2021-10-08 12:26:14 +0700

    download project

---
+++ app/js/main.js
@@ -0,0 +1,33 @@
+$(function () {
+  $('.discounts-slider__inner').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><svg><use xlink:href="images/sprite.svg#arrow-gray-l"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><svg><use xlink:href="images/sprite.svg#arrow-gray-r"></use></svg></button>',
+  });
+
+  $('.partners__list').slick({
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    arrows: false,
+    autoplay: true,
+  });
+
+  $('.product-top__star').rateYo({
+    starWidth: "16px",
+    numStars: 1,
+    ratedFill: "#FFB800",
+    normalFill: "#FFB800",
+    readOnly: true,
+  });
+
+  var containerEl1 = document.querySelector('[data-ref="product-top"]');
+  var containerEl2 = document.querySelector('[data-ref="stock"]');
+ 
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
+
+});
---
+++ gulpfile.js
@@ -0,0 +1,110 @@
+const {
+  src,
+  dest,
+  watch,
+  parallel,
+  series
+} = require('gulp');
+
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
+const image = require('gulp-image');
+const svgSprite = require('gulp-svg-sprite');
+const browserSync = require('browser-sync').create();
+const del = require('del');
+
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: 'app/'
+    },
+  });
+}
+
+const svgSprites = () => {
+  return src('app/images/svg/*.svg')
+  .pipe(svgSprite({
+    mode: {
+      stack: {
+        sprite: "../sprite.svg"
+      }
+    },
+  }))
+  .pipe(dest('app/images'));
+}
+
+function styles() {
+  return src('app/scss/style.scss')
+    .pipe(scss({
+      outputStyle: 'compressed'
+    }))
+    .pipe(concat('style.min.css'))
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 10 versions'],
+      grid: true
+    }))
+    .pipe(dest('app/css'))
+    .pipe(browserSync.stream());
+}
+
+function scripts() {
+  return src([
+      'node_modules/jquery/dist/jquery.js',
+      'node_modules/slick-carousel/slick/slick.js',
+      'node_modules/mixitup/dist/mixitup.js',
+      'node_modules/rateyo/src/jquery.rateyo.js',
+      'app/js/main.js'
+    ])
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream());
+}
+
+function images() {
+  return src('app/images/**/*.*')
+    .pipe(image({
+      pngquant: true,
+      optipng: false,
+      zopflipng: true,
+      jpegRecompress: false,
+      mozjpeg: true,
+      gifsicle: true,
+      svgo: true,
+      concurrent: 10,
+      quiet: true 
+    }))
+    .pipe(dest('dist/images'));
+}
+
+function cleanDist() {
+return del(['dist']);
+}
+
+function build() {
+  return src([
+    'app/**/*.html',
+    'app/css/style.min.css',
+    'app/js/main.min.js'
+  ], {base: 'app'})
+  .pipe(dest('dist'));
+}
+
+function watching() {
+  watch(['app/scss/**/*.scss'], styles);
+  watch(['app/images/svg/*.svg'], svgSprites);
+  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+  watch('app/**/*.html').on('change', browserSync.reload);
+}
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.browsersync = browsersync;
+exports.watching = watching;
+exports.images = images;
+exports.svgSprites = svgSprites;
+
+exports.build = series(cleanDist, images, svgSprites, build);
+exports.default = parallel(styles, scripts, svgSprites, browsersync, watching);
